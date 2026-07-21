3f1f4f725cd9be064616dbe1a1f24f9d961dc4cc
Kelvin <kelvin_abqory_35rpl@student.smktelkom-mlg.sch.id>
2026-07-21 14:11:14 +0700

    Add files via upload

---
+++ kelvin.js
@@ -0,0 +1,3 @@
+console.log("Nama : saya Kelvin Barra Abqory");
+console.log("SMP : MtsN 5 MALANG");
+console.log("Alasan saya masuk telkom adalah meningkatkan skill IT saya untuk karir di masa depan");
